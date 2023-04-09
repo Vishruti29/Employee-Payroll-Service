42b557f064fe2b1200108fb96297cfd40ff1e02e
Vishruti Patil <vishruti2908@gmail.com>
2023-04-09 13:21:44 +0530

    Employee Payroll Service for java file IO

---
+++ src/com/JavaIO/EmployeePayrollService.java
@@ -0,0 +1,6 @@
+package com.JavaIO;
+public class EmployeePayrollService {
+    public static void main(String[] args) {
+        System.out.println("Employee Payroll Service for java IO");
+    }
+}
