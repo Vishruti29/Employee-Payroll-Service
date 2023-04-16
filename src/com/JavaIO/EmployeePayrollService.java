ccc3afe6d08062166016ad3c3ffce897ed7b2f8e
Vishruti Patil <vishruti2908@gmail.com>
2023-04-16 07:54:42 +0530

    Created employee payroll and stored into file

--- src/com/JavaIO/EmployeePayrollService.java
+++ src/com/JavaIO/EmployeePayrollService.java
@@ -1,123 +1,65 @@
 package com.JavaIO;
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class EmployeePayrollService {
-    public static void main(String[] args) {
-        class Employee{
-            private String name;
-            private double salary;
-            private int age;
 
-            public Employee(String name, double salary, int age) {
-                this.name = name;
-                this.salary = salary;
-                this.age = age;
-            }
-            public String getName() {
-                return name;
-            }
-            public double getSalary() {
-                return salary;
-            }
-            public int getAge() {
-                return age;
-            }
+    public static void main(String[] args) throws IOException {
+        String fileName = "employee.txt";
+        List<EmployeePayrollData> employeePayrollDataList = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
 
-            @Override
-            public String toString() {
-                return "Employee{" + "name='" + name + '\'' + ", salary=" + salary + ", age=" + age + '}';
-            }
-        }
-        File file = new File("employeeData.txt"); // Check if file exists
-        if (file.exists()) {
-            System.out.println("File exists.");
-        } else {
-            System.out.println("File does not exist.");
-        }
-        Employee employee1 = new Employee("Vishruti", 50000.0, 25 );
-        Employee employee2 = new Employee("Ashu", 60000.0, 26 );
-        try{
-            FileWriter fileWriter = new FileWriter("employeeData.txt");
-            fileWriter.write(employee1.getName() + "," +employee1.getSalary() + "," +employee1.getAge()+ "\n");
-            fileWriter.write(employee2.getName() + "," +employee2.getSalary() + "," +employee2.getAge());
-            fileWriter.close();
-            System.out.println("Employee Data written to file successfully");
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
-        // Check if file exists
-        if (file.exists()) {
-            System.out.println("File exists.");
-        } else {
-            System.out.println("File does not exist.");
-        }
+        // Populate test employee payroll object
+        EmployeePayrollData employee1 = new EmployeePayrollData(101, "Garv", 50000);
+        EmployeePayrollData employee2 = new EmployeePayrollData(102, "Vish", 60000);
+        employeePayrollDataList.add(employee1);
+        employeePayrollDataList.add(employee2);
 
-        try{
-            FileReader fileReader = new FileReader("employeeData.txt");
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String line;
-            System.out.println("Employee Data read from file :");
-            while ((line = bufferedReader.readLine()) != null){
-                String[] data = line.split(",");
-                String name = data[0];
-                double salary = Double.parseDouble(data[1]);
-                int age = Integer.parseInt(data[2]);
-                Employee employee = new Employee(name,salary,age);
-                System.out.println(employee.toString());
-            }
-            bufferedReader.close();
-            fileReader.close();
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
-        // Delete file and check if it exists
-        if (file.delete()) {
-            System.out.println("File deleted successfully.");
-        } else {
-            System.out.println("Failed to delete file.");
-        }
-        if (file.exists()) {
-            System.out.println("File exists.");
-        } else {
-            System.out.println("File does not exist.");
-        }
-        // Create directory
-        File directory = new File("employeeDirectory");
-        if (!directory.exists()) {
-            if (directory.mkdir()) {
-                System.out.println("Directory created successfully.");
-            } else {
-                System.out.println("Failed to create directory.");
-            }
-        } else {
-            System.out.println("Directory already exists.");
-        }
-        // Create empty file in directory
-        File emptyFile = new File(directory, "emptyFile.txt");
-        try {
-            if (emptyFile.createNewFile()) {
-                System.out.println("Empty file created successfully.");
-            } else {
-                System.out.println("Failed to create empty file.");
+        // Write employee payroll to file
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            for (EmployeePayrollData employee : employeePayrollDataList) {
+                writer.write(employee.toString());
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        // List files and directories
-        System.out.println("Files and directories in current directory:");
-        String[] filesAndDirectories = new File(".").list();
-        for (String fileName : filesAndDirectories) {
-            System.out.println(fileName);
-        }
 
-        // List files with a specific extension
-        System.out.println("Files with extension .txt:");
-        File directoryFile = new File(".");
-        File[] fileList = directoryFile.listFiles((dir, name) -> name.toLowerCase().endsWith(".txt"));
-        Arrays.stream(fileList).forEach(fileItem -> System.out.println(fileItem.getName()));
+        // Count number of entries in file
+        int count = countEntries(fileName);
+        System.out.println("Number of entries in file: " + count);
+
+        scanner.close();
     }
+
+    private static int countEntries(String fileName) throws IOException {
+        int count = 0;
+        try (Scanner scanner = new Scanner(fileName)) {
+            while (scanner.hasNextLine()) {
+                scanner.nextLine();
+                count++;
+            }
+        }
+        return count;
+    }
+
 }
 
+class EmployeePayrollData {
+    private int id;
+    private String name;
+    private double salary;
+
+    public EmployeePayrollData(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String toString() {
+        return id + "," + name + "," + salary + "\n";
+    }
+}
